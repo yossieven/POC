084f5e65729bc2d571443f2022cdcc9ac2b78335
Yossi Even <yossi.even@gmail.com>
2020-09-20 10:40:29 +0300

    structural similiarity

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import cv2
-
+from skimage.metrics import structural_similarity as ssim
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -19,12 +20,32 @@ def mse(imageA, imageB):
     return err
 
 
+def compare_images(imageA, imageB, title):
+	# compute the mean squared error and structural similarity
+	# index for the images
+	m = mse(imageA, imageB)
+	s = ssim(imageA, imageB, multichannel=True)
+	# setup the figure
+	fig = plt.figure(title)
+	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+	# show first image
+	ax = fig.add_subplot(1, 2, 1)
+	plt.imshow(imageA, cmap = plt.cm.gray)
+	plt.axis("off")
+	# show the second image
+	ax = fig.add_subplot(1, 2, 2)
+	plt.imshow(imageB, cmap = plt.cm.gray)
+	plt.axis("off")
+	# show the images
+	plt.show()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     image = cv2.imread('generators/test_open_search_not_found.png')
-    image2 = cv2.imread('generators/test_voice_search_no_mic.png')
+    image2 = cv2.imread('generators/test_open_search_not_found.png')
     cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-    error = mse(image, image2)
+    error = compare_images(image, image2, "orig vs. orig")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
